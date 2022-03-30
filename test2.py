4b0cd10740788063f5283abd44e7570206337c76
Anoop Johny <57437701+flyfir248@users.noreply.github.com>
2022-03-30 17:52:59 +0200

    new patch

--- test2.py
+++ test2.py
@@ -1,3 +1,15 @@
-def print_twice(name='sam'):
-    print(name)
-    print(name)
+def print_twice(name):
+  print(name)
+  print(name)
+
+def printing_twice(part1, part2):
+  cat = part1 + part2
+  print_twice(cat)
+  
+# This function takes two arguments, concatenates them, and prints the result twice.
+
+line1 = 'Ping Ping '
+line2 = 'Pong Pong '
+printing_twice(line1, line2)
+
+print(cat)
